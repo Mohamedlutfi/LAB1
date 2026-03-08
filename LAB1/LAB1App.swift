906a1f22ad527fb143c763e8ca427f35699982de
Mohamed <Mohamelotfii@gmail.com>
2026-03-09 00:36:17 +0100

    Initial Commit

---
+++ LAB1/LAB1App.swift
@@ -0,0 +1,17 @@
+//
+//  LAB1App.swift
+//  LAB1
+//
+//  Created by Mohamed Lutfi Mohamed on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct LAB1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
